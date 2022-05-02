d60de2a7833483d8567803cd77ed0424f241a9ec
Stierlitz <stierlitzotto21@gmail.com>
2022-05-02 14:07:55 +0900

    Removing imperfections

---
+++ src/components/Auth_Page/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import "./index.scss";
+
+export const NotFound = () => {
+	return (
+		<div className="NotFound">
+			<span className="NotFound__error">404 Page not Found</span>
+		</div>
+	);
+};
---
+++ src/components/Page/LeftPage/index.jsx
@@ -0,0 +1,33 @@
+import "./index.scss";
+import {ButtonTurn} from "../../Repeat_components";
+
+export const LeftPage = () => {
+	return (
+		<div className="Block__Buttons">
+			<ButtonTurn
+				ClassButton={"ClassButton"}
+				ClassName={"ClassName"}
+				ClassChildren={"ClassChildren"}
+				Word={"Активные"}
+				Children={"Фотка"}
+				URL_LINK={"/Active"}
+			/>
+			<ButtonTurn
+				ClassButton={"ClassButton"}
+				ClassName={"ClassName"}
+				ClassChildren={"ClassChildren"}
+				Word={"Завершенные"}
+				Children={"Фотка"}
+				URL_LINK={"/Ending"}
+			/>
+			<ButtonTurn
+				ClassButton={"ClassButton"}
+				ClassName={"ClassName"}
+				ClassChildren={"ClassChildren"}
+				Word={"Сохраненные"}
+				Children={"Фотка"}
+				URL_LINK={"/Saving"}
+			/>
+		</div>
+	);
+};
